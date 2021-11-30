7b29ae67ea2fd6ad27c8a34433aff2d1e355636e
Ebubekir Demiray <64805742+ebdem@users.noreply.github.com>
2021-11-30 15:11:50 +0300

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,62 @@
+import React from "react";
+import App, { Container } from "next/app";
+import { Transition, config, animated } from "react-spring";
+import NProgress from "nprogress";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps, router: router };
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+    console.log(pageProps);
+    return (
+      <Container>
+        <Transition
+          keys={[router.asPath]}
+          native
+          from={
+            pageProps.transitionLeave || {
+              opacity: 0,
+              position: "absolute",
+              transform: "translateY(100px)"
+            }
+          }
+          enter={
+            pageProps.transitionEnter || {
+              opacity: 1,
+              position: "static",
+              transform: "translateY(0px)"
+            }
+          }
+          leave={
+            pageProps.transitionLeave || {
+              opacity: 0,
+              position: "absolute",
+              transform: "translateY(-100px)"
+            }
+          }
+        >
+          {styles => (
+            <animated.div style={{ width: "100vw", ...styles }}>
+              <Component {...pageProps} />
+            </animated.div>
+          )}
+        </Transition>
+      </Container>
+    );
+  }
+}
